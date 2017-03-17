39db210fa8060893de885d7fb031b9814c290446
tudeng <ubicray@gmail.com>
2017-03-17 13:50:22 +0200

    Progress makes better

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,6 +7,11 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu:/home/tudeng/elmar_robtech/devel/lib:/opt/ros/indigo/lib"
-export PKG_CONFIG_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/tudeng/elmar_robtech/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export CMAKE_PREFIX_PATH="/home/tudeng/elmar_robtech/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/tudeng/elmar_robtech/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/tudeng/elmar_robtech/devel/lib:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PKG_CONFIG_PATH="/home/tudeng/elmar_robtech/devel/lib/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/tudeng/elmar_robtech/build"
+export PYTHONPATH="/home/tudeng/elmar_robtech/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tudeng/elmar_robtech/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/tudeng/elmar_robtech/src:$ROS_PACKAGE_PATH"
